eaf9411e97de65006491eb0a02c883569767df73
GurpreetCS <er.gurpreet619@yahoo.com>
2020-02-08 10:06:50 +0530

    adding second changes to main.c

--- main.c
+++ main.c
@@ -6,6 +6,10 @@ int main()
 {
 	printf("Hello Gurpreet... Testing your git\n");
 	printf("Hello Gurpreet... Testing your git\n");
+	printf("Hello Gurpreet... Testing your git\n");
+	printf("Hello Gurpreet... Testing your git\n");
+	printf("Hello Gurpreet... Testing your git\n");
+	printf("Hello Gurpreet... Testing your git\n");
 	return 0;
 }
 
